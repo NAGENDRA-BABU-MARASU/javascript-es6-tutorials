882c56c0259cb3616212a4ff926b7d9541672240
Nagendra <nagendrababu.marasu@gmail.com>
2024-04-24 16:15:51 +0530

    classes and inheritance

--- src/main.js
+++ src/main.js
@@ -38,3 +38,40 @@ console.log(colors)
 
 colors = 'green'; // error 
 */
+
+/* ========================= classes & inheritance ==============
+class User{
+  constructor(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+
+  static countUsers(){
+    console.log('there are 50 users')
+  }
+
+  register(){
+    console.log(this.username + " is now registered");
+  }
+}
+
+// let bob = new User("bob", "bob@mail.com", "bob123")
+// bob.register();
+// User.countUsers();
+
+class Member extends User{
+  constructor(username, email, password, memberPackage){
+    super(username, email, password)
+    this.package = memberPackage;
+  }
+
+  getPackage(){
+    console.log(this.username + " is subscribed to the " + this.package + " package")
+  }
+}
+
+let mike = new Member("mike", "mike@mail.com", "123", "standard")
+mike.getPackage();
+mike.register();
+*/ 
