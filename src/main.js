e43c58b6d8791598e8f4ea83aff61c4a3d9a1a0a
Nagendra <nagendrababu.marasu@gmail.com>
2024-04-26 10:01:59 +0530

    generators

--- src/main.js
+++ src/main.js
@@ -263,3 +263,20 @@ console.log(carWeakMap)
 //   console.log(err)
 // })
 */
+
+function *g1(){
+  console.log('Hello');
+  yield 'Yield 1 Ran...'
+  console.log('World')
+  yield 'Yield 2 Ran...'
+  return 'Returned...'
+}
+
+var g = g1();
+// console.log(g.next().value)
+// console.log(g.next().value)
+// console.log(g.next())
+
+for(let val of g){
+  console.log(val)
+}
